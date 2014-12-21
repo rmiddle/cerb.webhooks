3fda759d67f7d1a05d075b2a19d8a997044ca0c6
Jeff Standen <jeff@webgroupmedia.com>
2014-12-21 15:37:10 -0800

    [Webhooks/Plugin] Implemented the page and menu item in Setup->Plugins

--- api/App.php
+++ api/App.php
@@ -22,3 +22,32 @@ class Controller_Webhooks implements DevblocksHttpRequestHandler {
 	function writeResponse(DevblocksHttpResponse $response) {
 	}
 };
+
+class Webhooks_SetupPageSection extends Extension_PageSection {
+	const ID = 'webhooks.setup.section';
+	
+	function render() {
+		$settings = DevblocksPlatform::getPluginSettingsService();
+		
+		$tpl = DevblocksPlatform::getTemplateService();
+	
+		$defaults = new C4_AbstractViewModel();
+		$defaults->class_name = 'View_WebhookHandler';
+		$defaults->id = 'setup_webhook_handlers';
+		
+		$view = C4_AbstractViewLoader::getView($defaults->id, $defaults);
+		$tpl->assign('view', $view);
+		
+		$tpl->display('devblocks:cerb.webhooks::setup/page.tpl');
+	}
+	
+};
+
+class Webhooks_SetupPluginsMenuItem extends Extension_PageMenuItem {
+	const ID = 'webhooks.setup.menu.plugins';
+	
+	function render() {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->display('devblocks:cerb.webhooks::setup/menu_item.tpl');
+	}
+};
---
+++ templates/setup/page.tpl
@@ -0,0 +1,3 @@
+<h2>{'webhooks.common.webhooks'|devblocks_translate|capitalize}</h2>
+
+{include file="devblocks:cerberusweb.core::internal/views/search_and_view.tpl" view=$view}
